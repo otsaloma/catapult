5c5a77ccb38ea33eab67569971badbab040d98e7
Osmo Salomaa <otsaloma@iki.fi>
2023-07-16 22:40:46 +0300

    Drop visible result count
    Since we can't center the whole window.

--- catapult/conf.py
+++ catapult/conf.py
@@ -51,7 +51,7 @@ class ConfigurationStore(catapult.DebugMixin):
 
     _defaults = {
         "max_results": 24,
-        "max_results_visible": 8,
+        "max_results_visible": 7,
         "plugins": ["apps", "builtins", "calculator", "clipboard", "files", "session"],
         "theme": "dark",
     }
