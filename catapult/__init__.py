d9bc4f8d25cc387d27c2745d44250d14a8388fae
Osmo Salomaa <otsaloma@iki.fi>
2024-04-03 22:18:57 +0300

    RELEASE 1.0.1

--- catapult/__init__.py
+++ catapult/__init__.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-__version__ = "1.0"
+__version__ = "1.0.1"
 
 import gi
 import logging
