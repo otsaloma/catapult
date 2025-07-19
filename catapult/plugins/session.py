74e25da3a124a25476d16cf79e029fe68ecf7d3b
Osmo Salomaa <otsaloma@iki.fi>
2025-07-20 02:38:16 +0300

    Remove unused import

--- catapult/plugins/session.py
+++ catapult/plugins/session.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-import itertools
 import subprocess
 
 from catapult.api import find_split_all
